f1746ccc9eacd3d5e55edfa3c5138845e4a9038f
Gabe Koss <gabe@gabekoss.com>
2013-08-12 13:27:38 -0400

    disconnect and close stream

--- rails_app/app/controllers/streamz_controller.rb
+++ rails_app/app/controllers/streamz_controller.rb
@@ -1,10 +1,15 @@
 class StreamzController < ApplicationController
   include ActionController::Live
+
   def send_command
     response.headers['Content-Type'] = 'text/event-stream'
     5.times {
       response.stream.write "command!\n"
     }
+  rescue IOError
+    # client disconneted
+  ensure
     response.stream.close
   end
+
 end
